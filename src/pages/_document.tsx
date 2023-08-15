de9b04d2f18a57e2410367d5f0cda0156486960f
Allan Prado <allanp.vba@gmail.com>
2023-08-15 01:35:08 -0300

    fix: metatags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,17 +34,34 @@ export default class MyDocument extends Document {
           />
 
           <meta name="author" content="Allan Prado" />
-          <meta property="description" content="Allan Prado - CEO of prdev portfolio" />
+          <meta
+            property="description"
+            content="Olá! Eu sou Allan Prado, o CEO da PRDev Portfolio. Nossa paixão está em criar soluções de software inovadoras. Venha explorar nosso portfólio e descubra como estamos moldando o mundo digital."
+          />
+          <meta
+            name="keywords"
+            content="desenvolvedor de software, programador de aplicativos, desenvolvimento de aplicativos móveis, desenvolvimento de aplicativos web, programação de sistemas, engenheiro de software, linguagens de programação, arquitetura de software, interface de usuário, experiência do usuário, desenvolvimento front-end, desenvolvimento back-end, codificação, soluções de software, inovação tecnológica, consultoria em software"
+          />
 
-          <meta property="twitter:title" content="Portfolio | Allan Prado"></meta>
-          <meta property="twitter:description" content="Allan Prado - CEO of prdev portfolio"></meta>
-          <meta property="twitter:image" content="/assets/img/aboutme.jpg"></meta>
-          <meta property="twitter:card" content="summary_large_image"></meta>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-          <meta property="og:title" content="Portfolio | Allan Prado"></meta>
-          <meta property="og:description" content="Allan Prado - CEO of prdev portfolio" />
-          <meta property="og:image" content="/assets/img/aboutme.jpg"></meta>
-          <meta property="og:url" content="https://portfolio.prdev.com.br/"></meta>
+          <meta name="robots" content="index, follow" />
+
+          <meta property="twitter:title" content="Portfolio | Allan Prado" />
+          <meta
+            property="twitter:description"
+            content="Sou Allan Prado, CEO da PRDev, uma empresa apaixonada por desenvolvimento de software e inovação tecnológica. Explore nosso portfólio para descobrir como transformamos ideias em soluções digitais poderosas."
+          />
+          <meta property="twitter:image" content="/assets/img/aboutme.jpg" />
+          <meta property="twitter:card" content="summary_large_image" />
+
+          <meta property="og:title" content="Portfolio | Allan Prado" />
+          <meta
+            property="og:description"
+            content="Sou Allan Prado, CEO da PRDev, uma empresa apaixonada por desenvolvimento de software e inovação tecnológica. Explore nosso portfólio para descobrir como transformamos ideias em soluções digitais poderosas."
+          />
+          <meta property="og:image" content="/assets/img/aboutme.jpg" />
+          <meta property="og:url" content="https://portfolio.prdev.com.br/" />
         </Head>
 
         <body>
